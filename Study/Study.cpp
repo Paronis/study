c368980d50dfdf31026d0d2b0e69d933b0bbac59
Paronis <thedonis.19@gmail.com>
2025-07-16 20:54:00 +0330

    setup information

--- Study/Study.cpp
+++ Study/Study.cpp
@@ -3,11 +3,53 @@
 
 using namespace std;
 
+int CalcAge(int year);
+void ConfirmAge();
+void EntryInformation();
 
+string Fname, Lname;
+int Age;
+int CurYear = 2025;
 
 int main()
 {
-    cout << "Hello World!\n";
+    EntryInformation();
+    ConfirmAge();
 
-    return 1;
+    return 0;
 }
+
+
+int CalcAge(int year)
+{
+    int age = CurYear - year;
+    return age;
+}
+
+void ConfirmAge()
+{
+    if (Age >= 18)
+    {
+        cout << "all done";
+
+    }
+    else
+    {
+        cout << "under age";
+    }
+
+}
+
+void EntryInformation()
+{
+    cout << "enter your name: ";
+    cin >> Fname;
+    cout << "enter your last name: ";
+    cin >> Lname;
+
+    int birthYear;
+    cout << "enter your birth year: ";
+    cin >> birthYear;
+    Age = CalcAge(birthYear);
+}
+
